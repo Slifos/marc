8558badbe11549a711a7c6da784ba164ce7944c6
Slifos <97668299+Slifos@users.noreply.github.com>
2024-11-13 10:36:28 +0100

    Add files via upload

---
+++ fonction.c
@@ -0,0 +1,9 @@
+typedef struct
+{
+
+}tree;
+
+typedef struct s_nnode
+{
+  T values;
+} t_nnode;
---
+++ fonction.h
@@ -0,0 +1,9 @@
+
+
+#ifndef UNTITLED1_STACK_H
+#define UNTITLED1_STACK_H
+
+
+
+
+#endif //UNTITLED1_STACK_H
